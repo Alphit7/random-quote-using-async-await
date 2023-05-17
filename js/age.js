6d39e8318bb26385926705f443d31fcd20c5093d
Alphit7 <jacobs.sylvain@gmail.com>
2023-05-17 16:12:44 +0200

    all done except for the loader

---
+++ js/age.js
@@ -0,0 +1,13 @@
+let quoteAge = document.querySelector(".Quote__Age");
+import { quoteFirstName } from "./fetchQuote.js";
+
+export function fetchAge(quoteFirstName) {
+  fetch("https://api.agify.io/?name=" + quoteFirstName)
+    .then((response) => response.json())
+    .then((json) => {
+      quoteAge.textContent = "That person's estimated age is: " + json.age;
+    })
+    .catch((error) => {
+      alert("There was an error!", error);
+    });
+}
